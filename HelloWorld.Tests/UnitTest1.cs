72e69d0707003ece884ce2ae2ae0e64d026c0e3f
munikoteswararao-chevuru <ch.mkr102@gmail.com>
2025-11-15 21:01:10 +0530

    Add code

---
+++ HelloWorld.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace HelloWorld.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
