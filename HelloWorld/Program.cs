9faceea5803fbf0ea0db0de8111928193daa54c4
munikoteswararao-chevuru <98310422+munikoteswararao-chevuru@users.noreply.github.com>
2025-11-30 18:31:02 +0530

    Update greeting message in Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from .NET Web API running in Kubernetes! with ArgoCD -Deployments ");
+app.MapGet("/", () => "Hello World from .NET Web API running in Kubernetes! with ArgoCD -Deployments :) ");
 
 app.Run();
 
