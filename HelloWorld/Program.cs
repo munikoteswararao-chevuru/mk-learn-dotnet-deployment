bb265d88c821a2bfcc10a1b413694a4a382c6078
munikoteswararao-chevuru <98310422+munikoteswararao-chevuru@users.noreply.github.com>
2025-12-14 16:27:50 +0530

    Add metrics info

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+/* Collect HTTP metrics automatically */
+app.UseHttpMetrics();
+
+/* Expose Prometheus metrics */
+app.MapMetrics();
+
 app.MapGet("/", () => "Hello World from .NET Web API running in Kubernetes! with ArgoCD -Deployments :( ");
 
 app.Run();
-
